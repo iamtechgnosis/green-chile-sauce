1f73860689b4dab78bf6c1f1ffdd8de44bc90740
Omar <valis452@gmail.com>
2022-01-02 16:39:09 -0700

    first commit

---
+++ green-chile.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Green Chile Sauce
+ * Plugin URI:      https://github.com/iamtechgnosis/green-chile-sauce
+ * Description:     WP Plugin that provides additonal CSS rules for use on existing Gutenburg blocks and includes things like Bootstrap Cards, Carousels, Buttons, Lists, and CSS animation and transitions.
+ * Author:          elysium001
+ * Author URI:      https://github.com/elysium001
+ * Text Domain:     green-chile
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Green_Chile
+ */
+
+// Your code starts here.
